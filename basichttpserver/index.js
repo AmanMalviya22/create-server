a4251978d8b6a5ee1650a9451a3c1bf73a3c11d0
Aman Malviya <amanaman26802@gmail.com>
2023-06-12 09:15:13 +0530

    reading and serving html form server

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,13 +1,23 @@
  const http=require("http");
  const port=8000;
-
+const fs=require('fs');
 
  function requestHandler(req,res){
     // console.log(req.url);
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end('<h1>Gotcah!</h1>');
- }
+    res.writeHead(200,{'Content-type':'text/html'});
+   //  res.end('<h1>Gotcah!</h1>');
 
+
+
+    fs.readFile('./index.html', function(err,data){
+      if(err){
+         console.log("error",err);
+         return res.end('<h1>Error</h1>');
+      }
+      return res.end(data);
+    })
+ }
+ 
  const server=http.createServer(requestHandler);
 
 
